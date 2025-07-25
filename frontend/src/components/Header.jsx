b59a1b8b8f9e9224824b460798477300087bbe35
rashimarasangi <rashimarasangi200@gmail.com>
2025-07-25 21:25:45 +0530

    speaciality menu is added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,7 +15,7 @@ With Trusted Doctors
     <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block'/>
 schedule your appointment hassle-free.</p>
 </div>
-<a href="" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>Book appoinment <img className='w-3' src={assets.arrow_icon} alt="" />
+<a href="#speciality" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>Book appoinment <img className='w-3' src={assets.arrow_icon} alt="" />
 </a>
       </div>
       {/*-------right side--------*/}
