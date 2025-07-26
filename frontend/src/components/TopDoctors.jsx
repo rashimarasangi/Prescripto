8027a1aa3ee6f422962062dfb2a0787a25648b7c
rashimarasangi <rashimarasangi200@gmail.com>
2025-07-26 11:39:04 +0530

    doctors section is added

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -14,7 +14,7 @@ const TopDoctors = () => {
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
       <div className='w-full grid gap-4 pt-5 gap-y-6 px-3 sm:px-0 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-        {doctors.slice(0,10).map((item,index)=>
+        {doctors.slice(0,10).map((item,index)=>(
         <div onClick={()=>navigate(`/appoinment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
             <img className='bg-blue-50' src={item.image} alt="" />
             <div className='p-4'>
@@ -26,7 +26,7 @@ const TopDoctors = () => {
             </div>
         </div>   
 
-        )}
+        ))}
       </div>
       <button onClick={()=>{navigate('/doctors');scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
