ed5d5c0a4cb720a3d22d7b9482b29b50fb1a374b
rashimarasangi <rashimarasangi200@gmail.com>
2025-10-09 11:50:05 +0530

    admin controller and doctor controller is added

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,32 @@
+//API for adding doctor
+
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
+    console.log({
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+      image,
+    });
+  } catch (error) {}
+};
+
+export { addDoctor };
